aa48788b323e8c7d1a19e4c72eb2aa415feb8775
blackkittenop <hardikthakre17@gmail.com>
2023-11-02 10:32:32 +0530

    Commited

---
+++ server/controllers/ImgSticher.js
@@ -0,0 +1,46 @@
+import imageSticher from "../models/imageSticher.js";
+
+export const addLinks = async (req,res) => {
+    const {image1,
+        image2,
+        image3,
+        image4} = req.body;
+
+    try {
+        const addImg = await imageSticher.create({
+            image1,
+            image2,
+            image3,
+            image4
+        })
+
+        res.status(200).json({
+            success:true,
+            message:"Link Uploaded Sucessfully",
+            addImg
+        })
+    } catch (error) {
+        res.status(404).json({
+            success:false,
+            message:error.message,
+        })
+    }
+}
+
+export const getLinks = async (req,res) => {
+    const id = req.params.id;
+    try {
+        const getImg = await imageSticher.find({_id:id});
+        res.status(200).json({
+            success:true,
+            message:"image fetch successfully",
+            getImg
+        })
+
+    } catch (error) {
+        res.status(404).json({
+            success:false,
+            message:error.message,
+        })
+    }
+}
--- server/routes/favourites.js
+++ server/routes/favourites.js
@@ -2,7 +2,6 @@ import express from "express";
 import { addFav, remFav , getFavProperties} from "../controllers/favourites.js";
 import authUser from "../middlewares/authUser.js";
 const router = express.Router();
-
 router.get("/getFav", authUser , getFavProperties);
 router.post("/addFav/:id", authUser, addFav);
 router.delete("/remFav/:id", authUser , remFav);
